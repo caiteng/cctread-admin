1500df3106bc2a95fdd0eb21014cdac0cd4cc909
caiteng <caiteng@192.168.1.121>
2018-01-05 16:15:15 +0800

    Initial commit

---
+++ src/main/java/com/cctread/CctreadAdminApplication.java
@@ -0,0 +1,12 @@
+package com.cctread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctreadAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctreadAdminApplication.class, args);
+	}
+}
